f41b5077fc179a977081b5be2c8661954a1ce25a
Eddie Marley <marley_eddie@hotmail.com>
2024-09-08 19:56:07 -0300

    Arquivo com Style.css

--- app/settings.py
+++ app/settings.py
@@ -77,15 +77,13 @@ WSGI_APPLICATION = "app.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'projectlds',
-        'USER': 'developer',
-        'PASSWORD': 'Msce1425',
-        'HOST': 'localhost',  # ou o IP do servidor do banco de dados
-        'PORT': '3306',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+
 
 
 # Password validation
